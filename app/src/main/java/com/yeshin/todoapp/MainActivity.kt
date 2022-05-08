c19ae81d2b3145636b2f1eb672f855bdb49179cc
silver0727 <syegreat94@gmail.com>
2022-05-08 23:43:38 +0900

    :pencil: 테스트 주석 삭제

--- app/src/main/java/com/yeshin/todoapp/MainActivity.kt
+++ app/src/main/java/com/yeshin/todoapp/MainActivity.kt
@@ -7,7 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        // Test  테스트입니당
     }
 }
