879bdff4097284c10e5c411cc66f0f246dc0159c
silver0727 <syegreat94@gmail.com>
2022-05-27 23:11:51 +0900

    MainActivity -> setup toolbar, nav_graph, drawerLayout

--- presentation/src/main/java/com/silver0727/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/silver0727/presentation/ui/main/MainActivity.kt
@@ -2,12 +2,17 @@ package com.silver0727.presentation.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.silver0727.presentation.R
+import com.silver0727.presentation.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
