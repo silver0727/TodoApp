a304aa3323371f312f8cc3ed55b11a4e3818a259
silver0727 <syegreat94@gmail.com>
2022-05-27 22:50:50 +0900

    필요 라이브러리 추가

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -10,26 +10,39 @@ object AppConfig {
 object Versions {
     const val gradle = "7.1.3"
     const val kotlin = "1.6.21"
-    // UI
+    const val coroutine = "1.5.0"
+    const val timber = "4.7.1"
+    // DI
+    const val hilt = "2.41"
+    // Presentation
     const val core_ktx = "1.7.0"
     const val appcompat = "1.4.1"
     const val constraintlayout = "2.1.3"
+    const val fragment = "1.4.1"
     const val navigation = "2.4.2"
     const val material = "1.6.0"
-    // DI
-    const val hilt = "2.28-alpha"
-    const val viewmodel_ktx = "2.4.1"
+    const val archLifecycle = "2.4.1"
+    // Data
+    const val room = "2.4.2"
+    // Domain
 }
 
 object Libraries {
     const val google_material = "com.google.android.material:material:${Versions.material}"
 
-    val viewModel by lazy { "androidx.lifecycle:lifecycle-viewmodel:${Versions.viewmodel_ktx}" }
+    object Lifecycle {
+        val process by lazy { "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.archLifecycle}" }
+        val runtime by lazy { "androidx.lifecycle:lifecycle-runtime-ktx:2.4.0-alpha01" }
+        val viewModel_ktx by lazy { "androidx.lifecycle:lifecycle-viewmodel-ktx:${Versions.archLifecycle}" }
+        val livedata_ktx by lazy { "androidx.lifecycle:lifecycle-livedata-ktx:${Versions.archLifecycle}" }
+        val compiler by lazy { "androidx.lifecycle:lifecycle-lifecycle-compiler:${Versions.archLifecycle}" }
+    }
 
     object AndroidX {
         val core_ktx by lazy { "androidx.core:core-ktx:${Versions.core_ktx}" }
         val appcompat by lazy { "androidx.appcompat:appcompat:${Versions.appcompat}" }
         val constaintlayout by lazy { "androidx.constraintlayout:constraintlayout:${Versions.constraintlayout}" }
+        val fragment_ktx by lazy { "androidx.fragment:fragment-ktx:${Versions.fragment}" }
         val navigation_fragment_ktx by lazy { "androidx.navigation:navigation-fragment-ktx:${Versions.navigation}" }
         val navigation_ui_ktx by lazy { "androidx.navigation:navigation-ui-ktx:${Versions.navigation}" }
     }
@@ -37,6 +50,23 @@ object Libraries {
     object Hilt {
         val android by lazy { "com.google.dagger:hilt-android:${Versions.hilt}" }
         val compiler by lazy { "com.google.dagger:hilt-android-compiler:${Versions.hilt}" }
+    }
+
+    object Room {
+        val runtime by lazy { "androidx.room:room-runtime:${Versions.room}" }
+        val compiler by lazy { "androidx.room:room-compiler:${Versions.room}" }
+        val ktx by lazy { "androidx.room:room-ktx:${Versions.room}" }
+    }
+
+    object Coroutine {
+        val core by lazy { "org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:${Versions.coroutine}" }
+    }
+
+    object Debugging {
+        val timber by lazy { "com.jakewharton.timber:timber:${Versions.timber}" }
+        val flipper by lazy { "com.facebook.flipper:flipper:0.147.0" }
+        val soloader by lazy { "com.facebook.soloader:soloader:0.10.1" }
+        val flipperNetworkPlugin by lazy { "com.facebook.flipper:flipper-network-plugin:0.147.0" }
     }
 }
 
