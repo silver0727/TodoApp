c10332e9b7ce453ca38f9d3ed560e52b96b0d9f2
silver0727 <syegreat94@gmail.com>
2022-05-27 22:53:18 +0900

    data module : room 라이브러리 추가

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -14,6 +14,12 @@ dependencies {
     implementation(Libraries.Hilt.android)
     kapt(Libraries.Hilt.compiler)
 
+    Libraries.Room.apply {
+        implementation(runtime)
+        kapt(compiler)
+        implementation(ktx)
+    }
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.3")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
