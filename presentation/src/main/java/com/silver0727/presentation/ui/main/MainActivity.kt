ffa3037dcc0956637a992e7dcc8559f21ab5c714
silver0727 <syegreat94@gmail.com>
2022-05-27 23:20:49 +0900

    MainActivity : setup Toolbar, DrawerLayout, NavController

--- presentation/src/main/java/com/silver0727/presentation/ui/main/MainActivity.kt
+++ presentation/src/main/java/com/silver0727/presentation/ui/main/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.silver0727.presentation.ui.main
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.silver0727.presentation.R
 import com.silver0727.presentation.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -11,8 +18,38 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private val navController: NavController by lazy {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navHostFragment.navController
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.toolbar)
+        setupNavigationDrawer()
+        appBarConfiguration =
+            AppBarConfiguration.Builder(setOf(R.id.tasks_fragment_dest, R.id.statistics_fragment_dest))
+                .setOpenableLayout(binding.drawerLayout)
+                .build()
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp()
+    }
+
+    private fun setupNavigationDrawer() {
+        binding.drawerLayout.apply {
+            setStatusBarBackground(R.color.purple_200)
+        }
     }
 }
+
+// Keys for navigation
+const val ADD_EDIT_RESULT_OK = Activity.RESULT_FIRST_USER + 1
+const val DELETE_RESULT_OK = Activity.RESULT_FIRST_USER + 2
+const val EDIT_RESULT_OK = Activity.RESULT_FIRST_USER + 3
