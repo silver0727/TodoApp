0fc48bdc95d45148147c6f0569f2b942172a169b
silver0727 <syegreat94@gmail.com>
2022-05-29 22:01:56 +0900

    TaskDetail 이벤트 정의

--- presentation/src/main/java/com/silver0727/presentation/model/navigation/NavigationDestination.kt
+++ presentation/src/main/java/com/silver0727/presentation/model/navigation/NavigationDestination.kt
@@ -3,4 +3,8 @@ package com.silver0727.presentation.model.navigation
 sealed class NavigationDestination {
     object Tasks: NavigationDestination()
     object AddTask: NavigationDestination()
+    data class TaskDetail(
+        val taskId: String,
+        val title: String
+    ): NavigationDestination()
 }
