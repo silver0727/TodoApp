c0d47514034946138bcb76fe4a8593e2364235ac
silver0727 <syegreat94@gmail.com>
2022-05-29 21:50:55 +0900

    각 레이어에 맞는 객체 상속

--- domain/src/main/java/com/silver0727/domain/dto/TaskDTO.kt
+++ domain/src/main/java/com/silver0727/domain/dto/TaskDTO.kt
@@ -1,8 +1,10 @@
 package com.silver0727.domain.dto
 
+import com.silver0727.domain.base.DTO
+
 data class TaskDTO(
     val id: String,
     val title: String,
     val description: String,
     val isCompleted: Boolean,
-)
+): DTO()
--- presentation/src/main/java/com/silver0727/presentation/model/task/TaskItem.kt
+++ presentation/src/main/java/com/silver0727/presentation/model/task/TaskItem.kt
@@ -1,8 +1,10 @@
 package com.silver0727.presentation.model.task
 
+import com.silver0727.domain.base.Item
+
 data class TaskItem(
     val id: String,
     val title: String,
     val description: String,
     val isCompleted: Boolean,
-)
+): Item()
